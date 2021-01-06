391fb5dc9f5e6abeb1de3c5c5662a9261831baff
scalvetr <sergi.calvet@gmail.com>
2021-01-06 04:38:59 +0100

    hive docker image improvements

--- docker/hive/startup.sh
+++ docker/hive/startup.sh
@@ -10,9 +10,13 @@ cd /opt/hive
 echo "initializing schema (will fail if is has been already initialized)"
 ./bin/schematool -initSchema -dbType derby
 
-echo "starting metastore"
-nohup ./bin/hive --service metastore --hiveconf hive.server2.enable.doAs=false &
+echo "Starting hive metastore"
+nohup ./bin/hive --service metastore --hiveconf hive.server2.enable.doAs=false --hiveconf hive.log.file=hive-metastore.log &
 
-echo "starting hiveserver2"
-./bin/hive --service hiveserver2 --hiveconf hive.server2.enable.doAs=false
+METASTORE_PORT=${METASTORE_PORT:-"9083"}
+echo "Witing until hive metastore is up on port ${METASTORE_PORT}"
+/usr/local/bin/wait-fot-it.sh -t 120 -s localhost:${METASTORE_PORT} -- echo "hive metastore is up on port ${METASTORE_PORT}"
+
+echo "Starting hiveserver2"
+env SERVICE=hiveserver2;./bin/hive --service hiveserver2 --hiveconf hive.server2.enable.doAs=false
 
